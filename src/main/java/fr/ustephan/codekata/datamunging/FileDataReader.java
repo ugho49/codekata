1807d4bf5c835d6816249ecc69fafdb8e835251f
Ugho STEPHAN <stephan.ugho@gmail.com>
2019-04-09 23:19:47 +0200

    refacto file reader to handle big files

--- src/main/java/fr/ustephan/codekata/datamunging/FileDataReader.java
+++ src/main/java/fr/ustephan/codekata/datamunging/FileDataReader.java
@@ -1,20 +1,29 @@
 package fr.ustephan.codekata.datamunging;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class FileDataReader {
 
     public static List<String> getLines(final File file) {
-        try {
-            return Files.readAllLines(Paths.get(file.toURI()));
-        } catch (IOException e) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (Exception e) {
             System.err.println(e.getMessage());
+            lines = Collections.emptyList();
+        } finally {
+            System.out.println("close buffer");
         }
-        return Collections.emptyList();
+        return lines;
     }
 }
