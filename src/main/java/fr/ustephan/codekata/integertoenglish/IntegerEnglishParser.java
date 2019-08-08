26c6d59e26ae2afa2e724475851ef15b9b2eb8fd
Ugho STEPHAN <ustephan.prestataire@oui.sncf>
2019-08-08 17:20:40 +0200

    Init Integer to English KATA

---
+++ src/main/java/fr/ustephan/codekata/integertoenglish/IntegerEnglishParser.java
@@ -0,0 +1,16 @@
+package fr.ustephan.codekata.integertoenglish;
+
+/**
+ * @author ugho_stephan.
+ */
+public class IntegerEnglishParser {
+
+    String[] units = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    String[] tenUnits = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "seventeen", "eighteen", "nineteen"};
+    String[] decadeUnits = {"twenty", "thirty", "fourty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    String[] bigUnits = {"Hundred", "Thousand", "Million", "Billion"};
+
+    public static String numberToWords(int num) {
+        return "";
+    }
+}
---
+++ src/test/java/fr/ustephan/codekata/integertoenglish/IntegerEnglishParserTest.java
@@ -0,0 +1,10 @@
+package fr.ustephan.codekata.integertoenglish;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author ugho_stephan.
+ */
+class IntegerEnglishParserTest {
+
+}
